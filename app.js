ea2c40183e28a347ce72f409ed95196d599000b2
Matt Swanson <mdswanson@sep.com>
2011-05-28 11:43:40 -0400

    fixing non-backward compatible express.js method names; still broken for latest mongoose version

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var app = module.exports = express.createServer();
 // Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.use(express.bodyDecoder());
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
